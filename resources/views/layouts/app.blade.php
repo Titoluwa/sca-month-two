bb700e7a2283a082b30d09398feb64ffad357fd9
Titoluwa <titoluwaranti@gmail.com>
2022-08-23 14:40:45 +0100

    Welcome view change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-purple-100">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
